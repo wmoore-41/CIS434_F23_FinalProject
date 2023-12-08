8f2665307198fdaacb18dd1732cf3071f50eaa56
Darshan <darshanthakkar716@gmail.com>
2023-12-07 23:05:39 -0500

    Add toString Method

--- Ticket.java
+++ Ticket.java
@@ -52,4 +52,16 @@ public class Ticket {
         assignedAgents.clear();
     }
 
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Ticket ID: ").append(this.ticketID).append("\n");
+        res.append("Ticket Status: ").append(isOpen ? "Open" : "Closed").append("\n");
+        res.append("Title: ").append(this.title).append("\n");
+        res.append("Description: ").append(this.description).append("\n");
+        res.append("Requested by User: ").append(this.requester.username).append("\n");
+        res.append("Assigned Agents: ").append(this.assignedAgents).append("\n");
+        res.append("Due By: ").append(this.dueDate).append("\n");
+        return res.toString();
+    }
 }
